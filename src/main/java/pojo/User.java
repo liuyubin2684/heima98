3460779973672c6b298dd7bff1f8aefd63f2d9b4
lyb <912086287@qq.com>
2020-08-18 19:23:06 +0800

    12

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -1,6 +1,16 @@
 package pojo;
 
 public class User {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     private String name;
 
     public String getPassword() {
